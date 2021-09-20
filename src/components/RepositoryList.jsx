0d756d7838ddf7b31e577b39e4149cf43a29c185
mardsonferreira <mardsonferreira25@gmail.com>
2021-09-20 08:15:50 -0300

    Adding fast refresh

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,20 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'Unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform',
+}
+
+export function RepositoryList () {
+    return (
+        <section className="repository-list">
+            <h1> Repository list</h1>
+            <ul>
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+               <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
